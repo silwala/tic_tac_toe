b01cb7da762cdfb16cc22fd14f8ab896e01eefab
Aditya Silwal <aditsilwal@gmail.com>
2024-07-06 02:15:17 -0400

    add main method to test if Game class draws the board correctly

---
+++ tic_tac_toe.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require_relative 'player'
+require_relative 'game'
+
+puts "enter player1's name"
+player1_name = gets.chomp
+
+puts "enter player1's symbol"
+
+is_valid_symbol = false
+until is_valid_symbol
+  begin
+    player1_symbol = gets.chomp.upcase
+    player1 = Player.new(player1_name, player1_symbol)
+  rescue ArgumentError => e
+    puts e.message
+  else
+    is_valid_symbol = true
+  end
+end
+
+
+puts "enter player2's name"
+player2_name = gets.chomp
+
+puts "enter player2's symbol"
+is_valid_symbol = false
+until is_valid_symbol
+  begin
+    player2_symbol = gets.chomp.upcase
+    player2 = Player.new(player2_name, player2_symbol)
+  rescue ArgumentError => e
+    puts e.message
+  else
+    is_valid_symbol = true
+  end
+end
+
+
+game = Game.new(player1, player2)
+game.play
